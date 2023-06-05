f4a3c1ffcc6adaa1d749789f55375187e3590d40
rafael <rafael.ldietrich@gmail.com>
2023-06-05 20:00:34 -0300

    Fix broken class.

--- src/main/kotlin/com/dietrich/futsal/FutsalApplication.kt
+++ src/main/kotlin/com/dietrich/futsal/FutsalApplication.kt
@@ -1,18 +1,11 @@
 package com.dietrich.futsal
 
 import com.dietrich.futsal.component.JwtProperties
-import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.security.authentication.AuthenticationProvider
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 
 @SpringBootApplication
-@OpenAPIDefinition
 @EnableConfigurationProperties(JwtProperties::class)
 class FutsalApplication
 
